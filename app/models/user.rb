11faf70c7b722265dd18d007ff3f0f17e4bd6a3e
Lucas Machado Cid <lucas_mcid@hotmail.com>
2020-03-21 10:21:33 -0300

    friend relation added

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,32 @@ class User < ApplicationRecord
   has_many :posts
   has_many :commentaries
   has_many :reactions
+
+  has_many :friendships
+  has_many :friends, :through => :friendships
+
+  has_many :inverse_friendships, :class_name => "Friendship", :foreign_key => "friend_id"
+  has_many :inverse_friends, :through => :inverse_friendships, :source => :user
+
+  def all_friends
+  	self.friends + self.inverse_friends
+  end
+
+  def confirmated_friends
+  	friends_array = friendships.map{|friendship| friendship.friend if friendship.acceptedRequest}
+    friends_array + inverse_friendships.map{|friendship| friendship.user if friendship.acceptedRequest}
+  end
+
+  def confirm_friend(user)
+    Friendship.where(user_id:user.id, friend_id:self.id).update(acceptedRequest:true)
+  end
+
+  def pending_friends
+    friendships.map{|friendship| friendship.friend if !friendship.acceptedRequest}
+  end
+
+  def friend_requests
+    inverse_friendships.map{|friendship| friendship.user if !friendship.acceptedRequest}
+  end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_20_145207) do
+ActiveRecord::Schema.define(version: 2020_03_21_120235) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2020_03_20_145207) do
     t.integer "reactionType3", default: 0
     t.integer "reactionType4", default: 0
     t.integer "dislikes", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "friend_id"
+    t.boolean "acceptedRequest", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
