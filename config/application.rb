ffb62c281726a7ba80497063242a97ce4697e561
Lucas Machado Cid <lucas_mcid@hotmail.com>
2020-03-23 20:41:33 -0300

    change extension

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module SocialNetwork
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
-	config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/.
+	config.assets.precompile += %w(.svg .eot .woff .ttf)
 
 
 
