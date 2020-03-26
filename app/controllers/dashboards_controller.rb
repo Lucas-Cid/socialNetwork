1bc10aa1d9320b2e586468cb999fa596f6e5599d
Lucas Machado Cid <lucas_mcid@hotmail.com>
2020-03-26 17:31:22 -0300

    adjusted the page layout and added some functions

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,21 @@
 class DashboardsController < ApplicationController
 
 	def homepage
+		@postsType = Post.all
+	end
+
+	def myProfile
+		@user = current_user
+		@postsType = current_user.posts
+		render :profile
+	end
+
+	def userProfile
+		@user = User.find(params.require(:id))
+		@postsType = @user.posts
+		render :profile
+	end
+
+	def messages
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "dashboards#homepage"
   get "users/addFriend", to: "users#addFriend"
+  get "dashboards/myProfile", to: "dashboards#myProfile"
+  get "dashboards/userProfile", to: "dashboards#userProfile"
+  get "dashboards/messages", to: "dashboards#messages"
   devise_for :users
   resources :posts, :reactions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
