ba3e3832ca2c3f883f3eea6c92f5361b8fedf0f6
Lucas Machado Cid <lucas_mcid@hotmail.com>
2020-03-27 10:30:55 -0300

    added profile layout

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,18 +1,28 @@
 class DashboardsController < ApplicationController
 
 	def homepage
-		@postsType = Post.all
+		@postsType = Post.all.order(:id).last(20).reverse
 	end
 
 	def myProfile
 		@user = current_user
-		@postsType = current_user.posts
+		@postsType = current_user.posts.order(:id).last(20).reverse
 		render :profile
 	end
 
 	def userProfile
 		@user = User.find(params.require(:id))
-		@postsType = @user.posts
+		@postsType = @user.posts.order(:id).last(20).reverse
+		render :profile
+	end
+
+	def userReactions
+		@user = User.find(params.require(:id))
+		@reactions = @user.reactions.order(:id).last(20).reverse
+		@postsType = []
+		@reactions.each do |reaction|
+			@postsType << Post.find(reaction.owner_id)
+		end
 		render :profile
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "dashboards/myProfile", to: "dashboards#myProfile"
   get "dashboards/userProfile", to: "dashboards#userProfile"
   get "dashboards/messages", to: "dashboards#messages"
+  get "dashboards/userReactions", to: "dashboards#userReactions"
   devise_for :users
   resources :posts, :reactions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
