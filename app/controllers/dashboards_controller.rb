8c59004e788141f3fb6c796706f1c2aa134bf2d2
Lucas Machado Cid <lucas_mcid@hotmail.com>
2020-03-28 20:48:36 -0300

    now, if the user react a post, the reaction button will become blue

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,17 +2,23 @@ class DashboardsController < ApplicationController
 
 	def homepage
 		@postsType = Post.all.order(:id).last(20).reverse
+		@reactionsTimeline = Reaction.where(owner_id:@postsType, user_id:current_user, owner_type:"Post")
+
 	end
 
 	def myProfile
 		@user = current_user
 		@postsType = current_user.posts.order(:id).last(20).reverse
+		@reactionsTimeline = Reaction.where(owner_id:@postsType, user_id:current_user, owner_type:"Post")
+
 		render :profile
 	end
 
 	def userProfile
 		@user = User.find(params.require(:id))
 		@postsType = @user.posts.order(:id).last(20).reverse
+		@reactionsTimeline  = Reaction.where(owner_id:@postsType, user_id:current_user, owner_type:"Post")
+
 		render :profile
 	end
 
@@ -21,14 +27,18 @@ class DashboardsController < ApplicationController
 		@reactions = @user.reactions.order(:id).last(20).reverse
 		@postsType = []
 		@reactions.each do |reaction|
-			@postsType << Post.find(reaction.owner_id)
+		@postsType << Post.find(reaction.owner_id)
+
 		end
+		@reactionsTimeline = Reaction.where(owner_id:@postsType, user_id:current_user, owner_type:"Post")
 		render :profile
 	end
 
 	def showPost
 		@post = Post.find(params.require(:post_id))
 		@commentaries = @post.commentaries.order(:id)
+		@postsType = @commentaries
+		@reactionsTimeline = Reaction.where(owner_id:@postsType, user_id:current_user, owner_type:"Commentary")
 
 		  respond_to do |format|
 
