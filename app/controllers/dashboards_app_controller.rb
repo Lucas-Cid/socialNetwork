2950e38c9af1397fa8ba23394083fae2d62bd1e0
Lucas Machado Cid <lucas_mcid@hotmail.com>
2020-03-30 15:25:15 -0300

    added controllers for app mobile

--- app/controllers/dashboards_app_controller.rb
+++ app/controllers/dashboards_app_controller.rb
@@ -31,57 +31,14 @@ class DashboardsAppController < ApplicationController
 			posts.push(postHash)
 		end
 
+		postsReturn = {
+						:posts => posts
+					  }
+
 		respond_to do |format|
-			format.json  { render :json => posts }
+			format.json  { render :json => postsReturn }
 		end
 
 	end
-
-	def myProfile
-		@user = current_user
-		@postsType = current_user.posts.order(:id).last(20).reverse
-		@reactionsTimeline = Reaction.where(owner_id:@postsType, user_id:current_user, owner_type:"Post")
-
-		render :profile
-	end
-
-	def userProfile
-		@user = User.find(params.require(:id))
-		@postsType = @user.posts.order(:id).last(20).reverse
-		@reactionsTimeline  = Reaction.where(owner_id:@postsType, user_id:current_user, owner_type:"Post")
-
-		render :profile
-	end
-
-	def userReactions
-		@user = User.find(params.require(:id))
-		@reactions = @user.reactions.order(:id).last(20).reverse
-		@postsType = []
-		@reactions.each do |reaction|
-		@postsType << Post.find(reaction.owner_id)
-
-		end
-		@reactionsTimeline = Reaction.where(owner_id:@postsType, user_id:current_user, owner_type:"Post")
-		render :profile
-	end
-
-	def showPost
-		@post = Post.find(params.require(:post_id))
-		@commentaries = @post.commentaries.order(:id)
-		@postsType = @commentaries
-		@reactionsTimeline = Reaction.where(owner_id:@postsType, user_id:current_user, owner_type:"Commentary")
-
-		  respond_to do |format|
-
-		    format.html
-
-		    format.js
-
-		  end
-
-	end
-
-
-
 
 end
